97cef706aded8b7c98f5f439979ad564c4abb0c8
grange <2634070476@qq.com>
2023-09-12 21:16:26 +0800

    half write lidar decoder

---
+++ src/tinker_lidar_processor/include/tinker_lidar_processor/lidar_decoder.hpp
@@ -0,0 +1,18 @@
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/point_cloud2.hpp"
+#include "sensor_msgs/msg/point_field.hpp"
+
+class LivoxMsgDecoder : public rclcpp::Node
+{
+public:
+   LivoxMsgDecoder(std::string name);
+
+private:
+    rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr command_subscribe_;
+    rclcpp::Publisher<sensor_msgs::msg::PointCloud>::SharedPtr command_publish_;
+    void subscribe_callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg);
+    void publish_callback(const sensor_msgs::msg::PointCloud::SharedPtr msg);
+    sensor_msgs::msg::PointCloud::SharedPtr converted_PointCloud;
+    rclcpp::TimerBase::SharedPtr timer_;
+}
+
