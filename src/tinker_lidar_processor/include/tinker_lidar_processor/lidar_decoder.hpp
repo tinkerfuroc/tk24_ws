a60b162f2997896dffa25a794628319b1f6a512f
grange <2634070476@qq.com>
2023-09-13 10:17:40 +0800

    lidar_decoder: read PointCloud2 and publish PointCloud

--- src/tinker_lidar_processor/include/tinker_lidar_processor/lidar_decoder.hpp
+++ src/tinker_lidar_processor/include/tinker_lidar_processor/lidar_decoder.hpp
@@ -1,5 +1,6 @@
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/point_cloud2.hpp"
+#include "sensor_msgs/msg/point_cloud.hpp"
 #include "sensor_msgs/msg/point_field.hpp"
 
 class LivoxMsgDecoder : public rclcpp::Node
@@ -11,8 +12,8 @@ private:
     rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr command_subscribe_;
     rclcpp::Publisher<sensor_msgs::msg::PointCloud>::SharedPtr command_publish_;
     void subscribe_callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg);
-    void timer_callback(const sensor_msgs::msg::PointCloud::SharedPtr msg);
-    sensor_msgs::msg::PointCloud::SharedPtr converted_PointCloud;
+    void timer_callback();
+    sensor_msgs::msg::PointCloud converted_PointCloud;
     rclcpp::TimerBase::SharedPtr timer_;
-}
+};
 
--- src/tinker_lidar_processor/src/lidar_decoder.cpp
+++ src/tinker_lidar_processor/src/lidar_decoder.cpp
@@ -1,16 +1,32 @@
-#include "tinker_lidar_processor/tinker_lidar_processor/include/tinker_lidar_processor/lidar_decoder.hpp"
+#include "tinker_lidar_processor/lidar_decoder.hpp"
 #include <sensor_msgs/msg/point_cloud.hpp>
 #include <sensor_msgs/msg/point_cloud.hpp>
 #include <sensor_msgs/point_cloud_conversion.hpp>
-LivoxMsgDecoder::LivoxMsgSubscriber(name)
+LivoxMsgDecoder::LivoxMsgDecoder(std::string name) : Node(name)
 {
-    RCLCPP_INFO(this->get_logger(), "%s node has started", name);
-    command_subscribe_ = this->create_subscription<sensor_msgs::msg::PointCloud2>("livox/lidar", 10, std::bind(&LivoxMsgDecoder::command_callback, this, std::placeholders::_1));
+    RCLCPP_INFO(this->get_logger(), "%s node has started", name.c_str());
+    command_subscribe_ = this->create_subscription<sensor_msgs::msg::PointCloud2>("livox/lidar", 10, std::bind(&LivoxMsgDecoder::subscribe_callback, this, std::placeholders::_1));
     command_publish_ = this->create_publisher<sensor_msgs::msg::PointCloud>("converted_lidar_pointcloud", 10);
     timer_ = this->create_wall_timer(std::chrono::milliseconds(500), std::bind(&LivoxMsgDecoder::timer_callback, this));
 }
 
 void LivoxMsgDecoder::subscribe_callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg)
 {
-    sensor_msgs::convertPointCloud2ToPointCloud(msg, converted_PointCloud);
+    sensor_msgs::convertPointCloud2ToPointCloud(*msg, converted_PointCloud);
+}
+
+void LivoxMsgDecoder::timer_callback()
+{
+    command_publish_->publish(converted_PointCloud);
+}
+
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+    /*创建对应节点的共享指针对象*/
+    auto node = std::make_shared<LivoxMsgDecoder>("Hello");
+    /* 运行节点，并检测退出信号*/
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
 }
