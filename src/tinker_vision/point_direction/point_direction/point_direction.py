1f19204344be4fbe53f818f11d9ee0d6e05cd61c
youw21@tinker <youw21@tinker.com>
2024-05-08 19:43:43 +0800

    finish point_direction

--- src/tinker_vision/point_direction/point_direction/point_direction.py
+++ src/tinker_vision/point_direction/point_direction/point_direction.py
@@ -4,13 +4,15 @@ from sensor_msgs.msg import Image
 from vision_msgs.msg import Direction
 from cv_bridge import CvBridge
 import mediapipe as mp
+import cv2
+import numpy as np
 
 def cross_product(p1, p2, p3):
     return (p2[0] - p1[0]) * (p3[1] - p1[1]) - (p3[0] - p1[0]) * (p2[1] - p1[1])
 
 def get_p1_p2(elbow_x, elbow_y, wrist_x, wrist_y, w, h):
     p1 = (wrist_x, wrist_y)
-    p2 = (0, 0)
+    p2 = (0.0, 0.0)
     if elbow_x < wrist_x:
         if elbow_y < wrist_y:
             if cross_product((elbow_x, elbow_y), p1, (w, h)) * cross_product((elbow_x, elbow_y), p1, (w, 0)) < 0:
@@ -68,28 +70,58 @@ class PointDirection(Node):
 
         if results_pose.pose_landmarks:
             # 左胳膊肘
-            left_elbow_x = int(results_pose.pose_landmarks.landmark[13].x * w1)
-            left_elbow_y = int(results_pose.pose_landmarks.landmark[13].y * h1)
+            left_elbow_x = float(results_pose.pose_landmarks.landmark[13].x * w1)
+            left_elbow_y = float(results_pose.pose_landmarks.landmark[13].y * h1)
 
             # 右胳膊肘
-            right_elbow_x = int(results_pose.pose_landmarks.landmark[14].x * w1)
-            right_elbow_y = int(results_pose.pose_landmarks.landmark[14].y * h1)
+            right_elbow_x = float(results_pose.pose_landmarks.landmark[14].x * w1)
+            right_elbow_y = float(results_pose.pose_landmarks.landmark[14].y * h1)
 
             # 左手腕
-            left_wrist_x = int(results_pose.pose_landmarks.landmark[15].x * w1)
-            left_wrist_y = int(results_pose.pose_landmarks.landmark[15].y * h1)
+            left_wrist_x = float(results_pose.pose_landmarks.landmark[15].x * w1)
+            left_wrist_y = float(results_pose.pose_landmarks.landmark[15].y * h1)
 
             # 右手腕
-            right_wrist_x = int(results_pose.pose_landmarks.landmark[16].x * w1)
-            right_wrist_y = int(results_pose.pose_landmarks.landmark[16].y * h1)
+            right_wrist_x = float(results_pose.pose_landmarks.landmark[16].x * w1)
+            right_wrist_y = float(results_pose.pose_landmarks.landmark[16].y * h1)
 
             left_p1, left_p2 = get_p1_p2(left_elbow_x, left_elbow_y, left_wrist_x, left_wrist_y, w1, h1)
-            self.results.left_p1_x, self.results.left_p1_y = left_p1
-            self.results.left_p2_x, self.results.left_p2_y = left_p2
+            print(left_p1, left_p2)
 
             right_p1, right_p2 = get_p1_p2(right_elbow_x, right_elbow_y, right_wrist_x, right_wrist_y, w1, h1)
-            self.results.right_p1_x, self.results.right_p1_y = right_p1
-            self.results.right_p2_x, self.results.right_p2_y = right_p2
+
+            point_img = np.copy(current_frame)
+            if results_pose.pose_landmarks:
+                point_img = cv2.circle(point_img, (int(left_elbow_x), int(left_elbow_y)), 5, (0, 0, 255), -1)
+                point_img = cv2.circle(point_img, (int(left_wrist_x), int(left_wrist_y)), 5, (1, 240, 255), -1)
+                point_img = cv2.circle(point_img, (int(right_elbow_x), int(right_elbow_y)), 5, (0, 0, 255), -1)
+                point_img = cv2.circle(point_img, (int(right_wrist_x), int(right_wrist_y)), 5, (1, 240, 255), -1)
+                point_img = cv2.line(point_img, (int(left_p1[0]), int(left_p1[1])), (int(left_p2[0]), int(left_p2[1])), (223, 155, 60), 3)
+                point_img = cv2.line(point_img, (int(right_p1[0]), int(right_p1[1])), (int(right_p2[0]), int(right_p2[1])), (94, 218, 121), 3)
+                point_img = cv2.putText(
+                    img=point_img,
+                    text=f"left point: {left_p1}, {left_p2}\nright point: {right_p1}, {right_p2}",
+                    # org=(int((y1 + y2) // 2), int((x1 + x2) // 2) + 25),
+                    org=(0, 45),
+                    fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                    fontScale=0.9,
+                    color=(0, 0, 255),
+                    thickness=2
+                )
+            cv2.imshow('point image', point_img)
+            cv2.waitKey(1)
+
+
+            self.results.left_p1_x, self.results.left_p1_y = float(left_p1[0]), float(left_p1[1])
+            self.results.left_p2_x, self.results.left_p2_y = float(left_p2[0]), float(left_p2[1])
+            self.results.right_p1_x, self.results.right_p1_y = float(right_p1[0]), float(right_p1[1])
+            self.results.right_p2_x, self.results.right_p2_y = float(right_p2[0]), float(right_p2[1])
+            
+        else:
+            self.results.left_p1_x, self.results.left_p1_y = (0.0, 0.0)
+            self.results.left_p2_x, self.results.left_p2_y = (0.0, 0.0)
+            self.results.right_p1_x, self.results.right_p1_y = (0.0, 0.0)
+            self.results.right_p2_x, self.results.right_p2_y = (0.0, 0.0)
 
 
 def main(args=None):
